be881cd094495ac6788f83ad630e76746b24ed87
Surya0191 <ssuryateja804@gmail.com>
2024-12-22 17:46:10 -0600

    Installed bootstrap, ngx-bootstrap, font-awesome packages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { appConfig } from './app.config';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { appConfig } from './app.config';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [{ provide: 'APP_CONFIG', useValue: appConfig }],
   bootstrap: [AppComponent]
